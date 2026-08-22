d9bc43075d96ca21b9c145328e3c51e16245b7ee
firebean <36950515+firebean850@users.noreply.github.com>
2026-08-22 14:21:05 +0800

    Echo commands
    Chatbot now echoes commands typed in by user and exits when user types in
    "bye" or "Bye".

--- src/main/java/Yun.java
+++ src/main/java/Yun.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
+
 public class Yun {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = "=".repeat(60) + "\n";
+
         String banner = "\\ \\ / /| | | || \\ | |\n"
               + " \\ V / | | | ||  \\| |\n"
               + "  | |  | |_| || |\\  |\n"
               + "  |_|   \\___/ |_| \\_|\n";
-        String line = "=".repeat(60) + "\n";
-        System.out.println(line + banner + "\nHello! I'm Yun.\nWhat can I do for you?\n" + line
-            + "Bye. Hope to see you again soon!\n" + line);
+        String intro = line + banner + "\nHello! I'm Yun.\nWhat can I do for you?\n" + line;
+        System.out.println(intro);
+        String exitMessage = "Bye. Hope to see you again soon!\n";
+        while (true) {
+            String command = scanner.nextLine();
+            System.out.println(line);
+            if (command.equals("bye") || command.equals("Bye")) {
+                System.out.println(exitMessage + "\n" + line);
+                break;
+            } else {
+                System.out.println(command + "\n\n" + line);
+            }
+        }
+        scanner.close();
     }
 }
