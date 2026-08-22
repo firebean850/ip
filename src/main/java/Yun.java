2b3179f66b6d007a73e8f99667dba5cb148c2b0b
firebean <36950515+firebean850@users.noreply.github.com>
2026-08-22 23:56:18 +0800

    Implement read/write for tasks in chatbot
    Added read/write functionality for tasks, users are able to see all tasks with "list" command;
    adding a new task will add it to the list.

--- src/main/java/Yun.java
+++ src/main/java/Yun.java
@@ -5,22 +5,37 @@ public class Yun {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line = "=".repeat(60) + "\n";
+        String[] tasklist = new String[100];
+        int count = 0;
 
         String banner = "\\ \\ / /| | | || \\ | |\n"
               + " \\ V / | | | ||  \\| |\n"
               + "  | |  | |_| || |\\  |\n"
               + "  |_|   \\___/ |_| \\_|\n";
+
         String intro = line + banner + "\nHello! I'm Yun.\nWhat can I do for you?\n" + line;
         System.out.println(intro);
         String exitMessage = "Bye. Hope to see you again soon!\n";
-        while (true) {
+        boolean running = true;
+        while (running) {
             String command = scanner.nextLine();
             System.out.println(line);
-            if (command.equals("bye") || command.equals("Bye")) {
-                System.out.println(exitMessage + "\n" + line);
-                break;
-            } else {
-                System.out.println(command + "\n\n" + line);
+            switch (command) {
+                case "bye":
+                case "Bye":
+                    System.out.println(exitMessage + "\n" + line);
+                    running = false;
+                    break;
+                case "list":
+                    for (int i = 0; i < count; i++) {
+                        System.out.printf("%d. %s%n", i+1, tasklist[i]);
+                    }
+                    System.out.println("\n" + line);
+                    break;
+                default:
+                    tasklist[count++] = command;
+                    System.out.println("added: " + command + "\n\n" + line);
+
             }
         }
         scanner.close();
