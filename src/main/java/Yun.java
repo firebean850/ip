de622b198d4ee372bac1f4003f11ab2b85191039
firebean <36950515+firebean850@users.noreply.github.com>
2026-08-22 13:58:32 +0800

    Initial commit: Set up Yun
    Added banner and intro message

---
+++ src/main/java/Yun.java
@@ -0,0 +1,11 @@
+public class Yun {
+    public static void main(String[] args) {
+        String banner = "\\ \\ / /| | | || \\ | |\n"
+              + " \\ V / | | | ||  \\| |\n"
+              + "  | |  | |_| || |\\  |\n"
+              + "  |_|   \\___/ |_| \\_|\n";
+        String line = "=".repeat(60) + "\n";
+        System.out.println(line + banner + "\nHello! I'm Yun.\nWhat can I do for you?\n" + line
+            + "Bye. Hope to see you again soon!\n" + line);
+    }
+}
